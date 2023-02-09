5ea25afe99a47e105e45acbb9bb101b6e3b465c4
DerekSilva97 <dereksilva194@gmail.com>
2023-02-08 21:41:39 -0300

    Created new component, node circle

---
+++ src/components/nodes/Circle.tsx
@@ -0,0 +1,30 @@
+import { NodeResizer } from "@reactflow/node-resizer";
+import { NodeProps, Handle, Position } from "reactflow";
+import '@reactflow/node-resizer/dist/style.css';
+
+
+export function Circle({selected}: NodeProps){
+    return(
+        <div className="bg-blue-500 w-full h-full rounded-full min-w-[200px] min-h-[200px] ">
+            <NodeResizer
+                minWidth={200}
+                minHeight={200}
+                isVisible={selected}
+                lineClassName="border-blue-400"
+                handleClassName="h-3 w-3 bg-white border-2 rounded border-blue-400"
+            />
+            <Handle 
+            id ='top' 
+            type="source" 
+            position={Position.Top}
+            className="-top-5 w-3 h-3 bg-blue-400/80 "
+            />
+            <Handle 
+            id ='bottom'
+            type="source"
+            position={Position.Bottom}
+            className="-bottom-5 w-3 h-3 bg-blue-400/80"
+            />
+        </div>
+    )
+}
